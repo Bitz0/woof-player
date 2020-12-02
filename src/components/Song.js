eaf5d9a56f63b8d9f009aa42dfd350ea5a0f386b
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-12-02 13:50:59 -0700

    Components draft

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Song name</h1>
+      <div>Artist</div>
+      <div>Picture</div>
+    </div>
+  )
+};
+
+export default Song;
