b2424d724648014a53cea5205ea02bb9cf05f713
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-12-02 14:30:42 -0700

    Adding icons to Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+  faPause,
+  faVolumeDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Player = () => {
   return (
@@ -9,7 +17,11 @@ const Player = () => {
         <input type="range" />
         <p>End Time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon className="skip-forward " size="2x" icon={faAngleRight} />
+      </div>
     </div>
   )
 };
