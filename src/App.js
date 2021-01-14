1764d9fbbd7330231d0f980ed5524693d450420f
Elizabeth Villalejos <nekobanpaia@gmail.com>
2021-01-14 14:52:59 -0700

    Adding nav component and toggling our library songs

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Song from './components/Song';
 import Library from './components/Library';
 import './styles/app.scss';
 import data from './data';
+import Nav from './components/Nav';
 function App() {
   const audioRef = useRef(null);
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
     const [songInfo, setSongInfo] = useState({ currentTime: 0, duration: 0 });
+	const [libraryStatus, setLibraryStatus] = useState(false);
   const timeUpdateHandler = (e) => {
 	const current = e.target.currentTime;
 	const duration = e.target.duration;
@@ -17,6 +19,7 @@ function App() {
 };
   return (
 		<div className='App'>
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
 			<Song currentSong={currentSong} />
 			<Player
 				setIsPlaying={setIsPlaying}
@@ -31,6 +34,7 @@ function App() {
 				songs={songs} setCurrentSong={setCurrentSong} 
 				isPlaying={isPlaying}
 				setSongs={setSongs}
+				libraryStatus={libraryStatus}
       />
 			<audio
 				onTimeUpdate={timeUpdateHandler}
